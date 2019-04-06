a13506a7c14ab2e5d75f16d6d1208c86ae80d826
Mark Edomwande <mark.edomwande@andela.com>
2019-04-06 22:29:43 +0100

    create server for deployment on heroku

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const http = require('http');
+const port = process.env.PORT || 8080;
+
+const app = express();
+app.server = http.createServer(app);
+
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+if (!module.parent) {
+  app.server.listen(port);
+  console.info(`Started on port ${port}`, 'info');
+}
+
+module.exports = app;
