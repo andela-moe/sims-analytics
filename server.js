5921613106f46aef8f76b209bbbee3f40117bee5
Mark Edomwande <mark.edomwande@andela.com>
2019-04-18 10:52:21 +0100

    removed prestart script

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const app = express();
 app.server = http.createServer(app);
 
 app.use(favicon(__dirname + '/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 
